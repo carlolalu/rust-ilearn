cd8790df69a2d29e5b34220ab5ac4ff0b50f2888
carlolalu <carlolanziluciani@gmail.com>
2024-03-18 12:23:02 +0100

    Official: ch12.3 - dealing with Config
    We refactor the arg parser as a COnfig constructor, so that the code might be
    easier to deal with (and test).

--- the-book/minigrep-chapter12/src/main.rs
+++ the-book/minigrep-chapter12/src/main.rs
@@ -12,35 +12,37 @@ use std::env;
 use std::fs;
 
 fn main() {
-    let args : Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-    let config = parse_args(&args);
+    let config = Config::new(&args);
 
-    println!("the query you gave is {}\nthe file_path you gace is {}\n\n", config.query, config.file_path);
+    println!(
+        "the query you gave is {}\nthe file_path you gace is {}\n\n",
+        config.query, config.file_path
+    );
 
     let contents = fs::read_to_string(config.file_path).expect("Could not read the given filepath");
 
-    println!("The given file_path points ot a file with contents {}", contents);
+    println!(
+        "The given file_path points ot a file with contents {}",
+        contents
+    );
 }
-
-
-// We can take another small step to improve the parse_config function further. At the moment, we’re returning a tuple, but then we immediately break that tuple into individual parts again. This is a sign that perhaps we don’t have the right abstraction yet. Another indicator that shows there’s room for improvement is the config part of parse_config, which implies that the two values we return are related and are both part of one configuration value. We’re not currently conveying this meaning in the structure of the data other than by grouping the two values into a tuple.
 
 struct Config {
     query: String,
     file_path: String,
 }
 
+// So now that the purpose of the parse_config function is to create a Config instance, we can change parse_config from a plain function to a function named new that is associated with the Config struct. Making this change will make the code more idiomatic.
 
-// The parse_config function then holds the logic that determines which argument goes in which variable and passes the values back to main. We still create the query and file_path variables in main, but main no longer has the responsibility of determining how the command line arguments and variables correspond.
+// carlolalu: I guess this will also make the code easier to test.
 
-// This rework may seem like overkill for our small program, but we’re refactoring in small, incremental steps. After making this change, run the program again to verify that the argument parsing still works. It’s good to check your progress often, to help identify the cause of problems when they occur.
-
-fn parse_args(args : &[String]) -> Config {
-
-    // We decide to clone for simplicity, but it could be implemented also with refs. In Chapter 13 there is a more efficient method to learn.
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config {query, file_path}
+impl Config {
+    fn new(args: &[String]) -> Config {
+        Config {
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+        }
+    }
 }
