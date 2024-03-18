2f4227ebaa1e73259b4387d64eefcb31144c6596
carlolalu <carlolanziluciani@gmail.com>
2024-03-18 12:41:53 +0100

    Official: ch12.3 - exctract logic from main()

--- the-book/minigrep-chapter12/src/lib.rs
+++ the-book/minigrep-chapter12/src/lib.rs
@@ -1,1 +1,34 @@
 
+use std::fs;
+use std::error::Error;
+
+// For the error type, we used the trait object Box<dyn Error> (and we’ve brought std::error::Error into scope with a use statement at the top). We’ll cover trait objects in Chapter 17. For now, just know that Box<dyn Error> means the function will return a type that implements the Error trait, but we don’t have to specify what particular type the return value will be. This gives us flexibility to return error values that may be of different types in different error cases.
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error> > {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!(
+        "The given file_path points ot a file with contents {}",
+        contents
+    );
+
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            Err("not enough arguments")
+        } else {
+            Ok(Config {
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+            })
+        }
+    }
+}
--- the-book/minigrep-chapter12/src/main.rs
+++ the-book/minigrep-chapter12/src/main.rs
@@ -1,21 +1,41 @@
 // Problems to address:
 
-//      3. The third problem is that we’ve used expect to print an error message when reading the file fails, but the error message just prints Should have been able to read the file. Right now, regardless of the situation, we’d print the same error message for everything, which wouldn’t give the user any information!
-
 //      4. Fourth, we use expect repeatedly to handle different errors, and if the user runs our program without specifying enough arguments, they’ll get an index out of bounds error from Rust that doesn’t clearly explain the problem. Having all the error-handling code in one place will also ensure that we’re printing messages that will be meaningful to our end users.
 
-use std::env;
-use std::fs;
+// Separation of Concerns for Binary Projects
 
-// We’ve added a new use line to bring process from the standard library into scope. The code in the closure that will be run in the error case is only two lines: we print the err value and then call process::exit. The process::exit function will stop the program immediately and return the number that was passed as the exit status code. This is similar to the panic!-based handling we used in Listing 12-8, but we no longer get all the extra output.
+// The organizational problem of allocating responsibility for multiple tasks to the main function is common to many binary projects. As a result, the Rust community has developed guidelines for splitting the separate concerns of a binary program when main starts getting large. This process has the following steps:
+
+//     Split your program into a main.rs and a lib.rs and move your program’s logic to lib.rs.
+//     As long as your command line parsing logic is small, it can remain in main.rs.
+//     When the command line parsing logic starts getting complicated, extract it from main.rs and move it to lib.rs.
+
+// The responsibilities that remain in the main function after this process should be limited to the following:
+
+//     Calling the command line parsing logic with the argument values
+//     Setting up any other configuration
+//     Calling a run function in lib.rs
+//     Handling the error if run returns an error
+
+// This pattern is about separating concerns: main.rs handles running the program, and lib.rs handles all the logic of the task at hand. Because you can’t test the main function directly, this structure lets you test all of your program’s logic by moving it into functions in lib.rs. The code that remains in main.rs will be small enough to verify its correctness by reading it. Let’s rework our program by following this process.
+
+use std::env;
 use std::process;
+
+// note, from chapter 7:
+
+//    We mentioned a package can contain both a src/main.rs binary crate root as well as a src/lib.rs library crate root, and both crates will have the package name by default. Typically, packages with this pattern of containing both a library and a binary crate will have just enough code in the binary crate to start an executable that calls code with the library crate. This lets other projects benefit from the most functionality that the package provides, because the library crate’s code can be shared.
+
+//    The module tree should be defined in src/lib.rs. Then, any public items can be used in the binary crate by starting paths with the name of the package. The binary crate becomes a user of the library crate just like a completely external crate would use the library crate: it can only use the public API. This helps you design a good API; not only are you the author, you’re also a client!
+
+//    In Chapter 12, we’ll demonstrate this organizational practice with a command-line program that will contain both a binary crate and a library crate.
+
+// This should explain why here we import with minigrep::
+
+use::minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    // To handle the error case and print a user-friendly message, we need to update main to handle the Result being returned by Config::build, as shown in Listing 12-10. We’ll also take the responsibility of exiting the command line tool with a nonzero error code away from panic! and instead implement it by hand. A nonzero exit status is a convention to signal to the process that called our program that the program exited with an error state.
-
-    // In this listing, we’ve used a method we haven’t covered in detail yet: unwrap_or_else, which is defined on Result<T, E> by the standard library. Using unwrap_or_else allows us to define some custom, non-panic! error handling. If the Result is an Ok value, this method’s behavior is similar to unwrap: it returns the inner value Ok is wrapping. However, if the value is an Err value, this method calls the code in the closure, which is an anonymous function we define and pass as an argument to unwrap_or_else. We’ll cover closures in more detail in Chapter 13.
 
     let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
@@ -27,29 +47,9 @@ fn main() {
         config.query, config.file_path
     );
 
-    let contents = fs::read_to_string(config.file_path).expect("Could not read the given filepath");
-
-    println!(
-        "The given file_path points ot a file with contents {}",
-        contents
-    );
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    // We can return a Result value that will contain a Config instance in the successful case and will describe the problem in the error case.
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            Err("not enough arguments")
-        } else {
-            Ok(Config {
-            query: args[1].clone(),
-            file_path: args[2].clone(),
-            })
-        }
+    // I guess we can see the function `run()` because it is a sibling of `Config`, and sibling are able to see in each others scope
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
