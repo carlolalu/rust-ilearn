df6f1deab7a53c46c3bfea5b558e3dda59684010
carlolalu <carlolanziluciani@gmail.com>
2024-05-20 23:26:48 +0200

    Grade-conversion
    Calculate my grade average is quite a complex task considering my situation.
    
    This is an embrio for what I should do. It works but it is definitely not
    prgrammed at its best.

---
+++ other-exercises/grading-conversion/src/lib.rs
@@ -0,0 +1,13 @@
+//! This library should help students of a double degree program to deal with their grades, split among different universities.
+//! The idea should be that in a future this should be a library where a student can charge its grading systems and its conversion tables
+//! between the grading systems, and then be able to have the correspondent translated grade. Furthermore, once this is obtained,
+//! the student should be able to use also his own defined closures (callbacks actually) so to calculate the averages of such grades
+//! according to the rules of each institution involved. The project is to this moment very raw.
+
+mod career;
+mod my_data;
+
+#[macro_use]
+extern crate lazy_static;
+
+
