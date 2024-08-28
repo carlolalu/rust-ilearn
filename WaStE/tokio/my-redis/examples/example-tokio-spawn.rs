094270818708fb916c6a5ae0007cfa927f983a3a
Carlo Lanzi Luciani <carlo.lanzi.luciani@intra2net.com>
2024-08-28 16:06:09 +0200

    Tokio tutorial: Hello Tokio + Spawning

---
+++ WaStE/tokio/my-redis/examples/example-tokio-spawn.rs
@@ -0,0 +1,24 @@
+use tokio::task;
+use tokio::task::yield_now;
+use std::rc::Rc;
+
+#[tokio::main]
+async fn main() {
+    let v = vec![1, 2, 3];
+
+    task::spawn(async move {
+        println!("Here's a vec: {:?}", v);
+    });
+
+    tokio::spawn(async {
+        // The scope forces `rc` to drop before `.await`.
+        {
+            let rc = Rc::new("hello");
+            println!("{}", rc);
+        }
+
+        // `rc` is no longer used. It is **not** persisted when
+        // the task yields to the scheduler
+        yield_now().await;
+    });
+}
